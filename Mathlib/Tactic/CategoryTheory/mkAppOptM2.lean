44014aca33f9726c0496b30e516efe6dd4f6be4b
Calle Sönne <calle.sonne@gmail.com>
2024-08-27 16:58:06 +0100

    progress

--- Mathlib/Tactic/CategoryTheory/mkAppOptM2.lean
+++ Mathlib/Tactic/CategoryTheory/mkAppOptM2.lean
@@ -1,9 +1,14 @@
 import Lean
+import Mathlib.CategoryTheory.Bicategory.Functor.Pseudofunctor
+import Mathlib.CategoryTheory.Category.Cat
 
-open Lean Meta Elab Tactic
+open Lean Meta Elab Tactic Qq
+
+universe v u
 
 namespace Test
 
+-- same here (as in function below)
 private def throwAppBuilderException {α} (op : Name) (msg : MessageData) : MetaM α :=
   throwError "AppBuilder for '{op}', {msg}"
 
@@ -14,6 +19,7 @@ private def mkFun (constName : Name) : MetaM (Expr × Expr) := do
   let fType ← instantiateTypeLevelParams cinfo us
   return (f, fType)
 
+-- I should just be able to use standard version of this?
 private def withAppBuilderTrace {α} {β} [ToMessageData α] [ToMessageData β]
     (f : α) (xs : β) (k : MetaM Expr) : MetaM Expr :=
   let emoji | .ok .. => checkEmoji | .error .. => crossEmoji
@@ -26,43 +32,56 @@ private def withAppBuilderTrace {α} {β} [ToMessageData α] [ToMessageData β]
       trace[Meta.appBuilder.error] ex.toMessageData
       throw ex
 
+-- This basically just tries to synthesize all mvars. Then
 private def mkAppMFinal (methodName : Name) (f : Expr) (args : Array Expr) (instMVars : Array MVarId) : MetaM Expr := do
-  instMVars.forM fun mvarId => do
-    let mvarDecl ← mvarId.getDecl
-    let mvarVal  ← synthInstance mvarDecl.type
-    mvarId.assign mvarVal
+  logInfo m!"f: {f}"
   let result ← instantiateMVars (mkAppN f args)
-  if (← hasAssignableMVar result) then throwAppBuilderException methodName ("result contains metavariables" ++ indentExpr result)
+  logInfo m!"result: {result}"
+  -- if (← hasAssignableMVar result) then throwAppBuilderException methodName ("result contains metavariables" ++ indentExpr result)
   return result
 
 private partial def mkAppOptMAux (f : Expr) (xs : Array (Option Expr)) : Nat → Array Expr → Nat → Array MVarId → Expr → MetaM Expr
+  -- if its a forall expression
   | i, args, j, instMVars, Expr.forallE n d b bi => do
+    -- substitute args into d (which is the type of the next argument?)
     let d  := d.instantiateRevRange j args.size args
+    -- if havent applied all arguments already (?)
     if h : i < xs.size then
       match xs.get ⟨i, h⟩ with
+      -- if we don't want to apply this argument
       | none =>
+        -- if its an instance argument, add to argument list + add to instMVar list
         match bi with
-        | BinderInfo.instImplicit => do
-          let mvar ← mkFreshExprMVar d MetavarKind.synthetic n
-          mkAppOptMAux f xs (i+1) (args.push mvar) j (instMVars.push mvar.mvarId!) b
+        -- | BinderInfo.instImplicit => do
+        --   let mvar ← mkFreshExprMVar d MetavarKind.synthetic n
+        --   mkAppOptMAux f xs (i+1) (args.push mvar) j (instMVars.push mvar.mvarId!) b
+        -- else add to argument list + proceed
         | _                       => do
           let mvar ← mkFreshExprMVar d MetavarKind.natural n
           mkAppOptMAux f xs (i+1) (args.push mvar) j instMVars b
+      -- if we want to apply this argument
       | some x =>
         let xType ← inferType x
+        -- TODO: whats d (type of next argument)
         if (← isDefEq d xType) then
           mkAppOptMAux f xs (i+1) (args.push x) j instMVars b
         else
           throwAppTypeMismatch (mkAppN f args) x
+    -- if all arguments have already been supplied?
     else
       mkAppMFinal `mkAppOptM f args instMVars
+  -- else
   | i, args, j, instMVars, type => do
     let type := type.instantiateRevRange j args.size args
     let type ← whnfD type
+    -- now it can somehow be a forall thing again?
     if type.isForall then
       mkAppOptMAux f xs i args args.size instMVars type
+    -- if we are on the final argument
     else if i == xs.size then
+      -- TODO: understand
       mkAppMFinal `mkAppOptM f args instMVars
+    -- TODO
     else do
       let xs : Array Expr := xs.foldl (fun r x? => match x? with | none => r | some x => r.push x) #[]
       throwAppBuilderException `mkAppOptM ("too many arguments provided to" ++ indentExpr f ++ Format.line ++ "arguments" ++ xs)
@@ -91,5 +110,11 @@ def mkAppOptM' (f : Expr) (xs : Array (Option Expr)) : MetaM Expr := do
   let fType ← inferType f
   withAppBuilderTrace f xs do withNewMCtxDepth do
     mkAppOptMAux f xs 0 #[] 0 #[] fType
+
+
+variable {C : Type u} [CategoryTheory.Bicategory C]
+
+#eval mkAppOptM ``CategoryTheory.Pseudofunctor.mapComp_assoc_right_hom
+  #[none, none, none, none, none, none, none, none, none, none, none, none]
 
 end Test
